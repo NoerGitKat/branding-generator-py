091eeeb3654cb4b00f68295df78bdfe1d3deff1e
Noer Paanakker <noer@lightworksweb.nl>
2023-06-24 00:48:48 +0200

    Added CORS access to web server

--- backend/app/api/routes/generate.py
+++ backend/app/api/routes/generate.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, HTTPException
 from modules import prompts, utils, constants
 
@@ -14,5 +15,5 @@ async def generate_snippet_api(prompt: str):
 async def generate_keywords_api(prompt: str):
     if not utils.validate_input_length(prompt):
         raise HTTPException(status_code=422, detail=f"Prompt is too long. Please use {constants.MAX_INPUT_SIZE} or less characters!")
-    keywords: str = prompts.generate_keywords(prompt)
-    return {"keywords": f"{keywords}"}
+    keywords: List[str] = prompts.generate_keywords(prompt)
+    return {"keywords": keywords}
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from api.routes import generate
 from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
 
 # START COMMAND: uvicorn api:app --reload
 app = FastAPI()
 handler = Mangum(app)
 
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"], 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"] 
+)
 app.include_router(generate.router, prefix="/api")
 
 @app.get("/")
